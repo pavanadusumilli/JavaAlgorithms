21accce5bb0eb49131196be33a059ae02459ed03
pavan <adusumilli.474@gmail.com>
2018-02-01 17:07:11 +0530

    First Commit

---
+++ binarySearch/src/binarySearch/binarySearch.java
@@ -0,0 +1,46 @@
+package binarySearch;
+
+import java.util.Scanner;
+
+public class binarySearch {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Please Enter the size of sorted input Array: ");
+		int len = s.nextInt();
+		int sortedArray[] = new int[len];
+		System.out.println("Enter the Array elements:");
+		for (int i = 0; i < len; i++) {
+			sortedArray[i] = s.nextInt();
+		}
+		System.out.println("Assuming the Enteres Array is printable and proceeding for the same.");
+		System.out.println("Please enter the element to find the position:");
+		int element = s.nextInt();
+		int position = binarySearch(sortedArray, len, element);
+		s.close();
+		System.out.println("The position of required element is ::"+position);
+	}
+
+	public static int binarySearch(int arr[], int len, int element) {
+		int startPosition = 0;
+		int midPosition = len / 2;
+		int lastPosition = len-1;
+		if (arr[startPosition] < element && arr[lastPosition] > element) {
+			do {
+				if (element > arr[midPosition]) {
+					startPosition = midPosition;
+				} else if (element < arr[midPosition]) {
+					lastPosition = midPosition;
+				} else {
+					return (midPosition);
+				}
+				midPosition = startPosition+(lastPosition-startPosition)/2;
+			} while (midPosition >= 0 || midPosition <= (len - 1));
+		}
+		else
+		{
+			return 0;
+		}
+		return 0;
+	}
+}
